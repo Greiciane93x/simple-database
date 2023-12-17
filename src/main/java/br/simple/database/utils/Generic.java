2b8d07ecbf372a79f6c701cacca6edb867c6db76
Greiciane93x <greiciane.assis@hotmial.com>
2023-12-17 20:47:15 -0300

    feature: refactoring rewrite on file

--- src/main/java/br/simple/database/utils/Generic.java
+++ src/main/java/br/simple/database/utils/Generic.java
@@ -1,20 +1,24 @@
-package br.simple.database.utils;
-
-public class Generic<T> {
-    T t;
-
-    public Generic(T t){
-        this.t  = t;
-    }
-
-    public T getGeneric(){
-        return this.t;
-    }
-
-    @Override
-    public String toString() {
-        return "Value{" +
-                "t=" + t +
-                '}';
-    }
-}
+//package br.simple.database.utils;
+//
+//public class Generic<T> {
+//    T t;
+//
+//    public Generic(T t){
+//        this.t  = t;
+//    }
+//
+//    public T getGeneric(){
+//        return this.t;
+//    }
+//
+//    public void setT(T t) {
+//        this.t = t;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Value{" +
+//                "t=" + t +
+//                '}';
+//    }
+//}
