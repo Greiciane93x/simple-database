36dad4f25673a9276ebe4e6a3f1ebbb45c511da0
Greiciane93x <greiciane.assis@hotmial.com>
2023-12-18 10:15:32 -0300

    feature: refactoring rewrite on file

--- src/main/java/br/simple/database/files/Read.java
+++ src/main/java/br/simple/database/files/Read.java
@@ -1,35 +1,19 @@
 package br.simple.database.files;
 
-import br.simple.database.domain.Details;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 import static br.simple.database.utils.DefaultManagementUtils.getContent;
-import static br.simple.database.utils.DefaultManagementUtils.getFile;
 
 
 public class Read {
 
     public static void main(String[] args) throws IOException {
 
-        ObjectMapper objectMapper = new ObjectMapper();
-
         Map content = getContent();
-
-        Map<String, Details> detailsMap = new HashMap<>();
-
-        Details personDetails6 = new Details.Builder().idPessoa(UUID.randomUUID().toString()).nome("tes2t").idade("29 anos").build();
-
-        Details put = detailsMap.put(personDetails6.getIdPessoa(), personDetails6);
-
-        objectMapper.writeValue(getFile(), put);
 
         System.out.println(content);
 
-
+        System.out.println("** -- READ -- **");
     }
 }
